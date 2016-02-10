d56ae0c9e7b0979aa5f1501bd3021b0d82971b52
Oren <orengolan@gmail.com>
2016-02-09 22:38:45 -0800

    misc

--- api.md
+++ api.md
@@ -7,7 +7,21 @@ returns ok.
 ===
 
 * Status: `200`
+* Content-Type: "text/plain; charset=utf-8"
 
 ```
 ok
+```
+
+## `GET /search`
+
+returns ok.
+
+===
+
+* Status: `200`
+* Content-Type: "application/json"
+
+```
+{"Name":"Alex","Hobbies":["snowboarding","programming"]}
 ```
--- main.go
+++ main.go
@@ -1,13 +1,20 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 )
 
+type Profile struct {
+	Name    string
+	Hobbies []string
+}
+
 func main() {
 	http.HandleFunc("/health", health)
+	http.HandleFunc("/search", search)
 
 	log.Println("server listening")
 	log.Fatal(http.ListenAndServe(":3000", nil))
@@ -15,5 +22,18 @@ func main() {
 
 func health(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
-	fmt.Fprintln(w, "ok")
+	fmt.Fprintf(w, "ok")
+}
+
+func search(w http.ResponseWriter, r *http.Request) {
+	profile := Profile{"Alex", []string{"snowboarding", "programming"}}
+
+	js, err := json.Marshal(profile)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(js)
 }
